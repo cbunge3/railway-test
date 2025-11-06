3f6fce68b10255a9c2331c73f13f81b0057fbead
cbunge3 <59035777+cbunge3@users.noreply.github.com>
2025-11-06 12:20:18 -0600

    Add hello.py to print greeting and timestamp

---
+++ hello.py
@@ -0,0 +1,8 @@
+import datetime
+
+def main():
+    print("Hello World from Railway!")
+    print(f"Script executed at: {datetime.datetime.now().isoformat()}")
+
+if __name__ == "__main__":
+    main()
